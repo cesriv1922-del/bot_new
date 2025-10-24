1d88228402edbb48444d847f321e336bbded040c
Cizar <ces.riv1922@gmail.com>
2025-10-24 01:49:25 -0500

    Add keepalive.py for Render Free

--- keepalive.py
+++ keepalive.py
@@ -8,5 +8,5 @@ def home():
     return "Bot is alive!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
